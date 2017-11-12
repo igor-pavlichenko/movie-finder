856b49d6c5aa8b50721d0b8952cb9a0fab5e27cb
Igor Pavlichenko <ig0r260892@gmail.com>
2017-11-12 23:31:02 +0000

    Add nav and footer to app's template
    Create a 'movies' component

---
+++ src/app/components/movies/movies.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class MoviesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
